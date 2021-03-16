c16e84c217f5ec9fc0eab3b45f8dccdec1fe6713
Desiree Lerma <desi.lerma96@yahoo.com>
2021-03-15 20:25:26 -0500

    Creates relationship between models, migrates db tables, runs rails scaffold.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :cases
+  resources :categories
+  # root 'categories#index'
+  # resources :categories do
+  #   resources :cases, only: [:index, :show]
+  # end
+
+  # resources :cases, only: [:new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
